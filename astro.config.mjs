f22ecbf0143318ac59c3d01a46197749fa763efe
Dolce&Beretta <brayanadanromeronavarro@gmail.com>
2026-02-28 23:04:53 -0700

    fix add @astrojs/tailwind and node

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+import node from "@astrojs/node";
 
-import react from '@astrojs/react';
-
-import tailwindcss from '@tailwindcss/vite';
+const baseUrl = import.meta.env.PUBLIC_BASE_URL;
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
-
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  output: "server",
+  base: baseUrl,
+  integrations: [tailwind(), react()],
+  adapter: node({
+    mode: "standalone",
+  }),
 });
