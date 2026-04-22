9232a876cb9d2ceb5c0891df873e8168ea9519ff
Dolce&Beretta <brayanadanromeronavarro@gmail.com>
2026-04-21 22:40:44 -0700

    add registro-evento

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,16 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import node from "@astrojs/node";
-
-import react from '@astrojs/react';
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  base: baseUrl,
-  integrations: [tailwind(), react()],
+  integrations: [react()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
   adapter: node({
     mode: "standalone",
   }),
